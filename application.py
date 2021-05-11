6dfd0053caf8b473633d59500c6121ef4d48e869
Talha Anwar <chtalhaanwar@gmail.com>
2021-05-12 04:35:57 +0500

    static file

--- application.py
+++ application.py
@@ -6,6 +6,7 @@ import tensorflow as tf
 #from tensorflow.keras.models import Sequential
 #from tensorflow.keras.layers import Dense, Dropout, Conv2D, MaxPooling2D, BatchNormalization, Flatten
 import numpy as np
+from whitenoise import WhiteNoise
 
 import os
 os.environ['KMP_DUPLICATE_LIB_OK']='True'
@@ -17,7 +18,8 @@ COUNT = 0
 
 app = Flask(__name__)
 app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 1
-
+static_dir = os.path.join(os.path.dirname(__file__), 'static')
+app.wsgi_app = WhiteNoise(app.wsgi_app, root=static_dir, prefix='static/')
 #render index.html as the first page 
 @app.route('/')
 def man():
